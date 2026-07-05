31d71725402ade7bfd1fdaf2ad6d92e954b05b45
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-05 17:40:35 +0200

    GeoJSON Ländergrenzen auf Weltkarte angezeigt

--- src/app/world-map/world-map.ts
+++ src/app/world-map/world-map.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-world-map',
@@ -8,6 +9,7 @@ import * as L from 'leaflet';
   styleUrl: './world-map.css',
 })
 export class WorldMap implements OnInit {
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     const map = L.map('map').setView([51.1657, 10.4515], 4);
@@ -15,5 +17,9 @@ export class WorldMap implements OnInit {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
       attribution: '© OpenStreetMap contributors'
     }).addTo(map);
+    
+    this.http.get('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').subscribe((geoData: any) => { 
+      L.geoJSON(geoData).addTo(map);
+    });
   }
 }
