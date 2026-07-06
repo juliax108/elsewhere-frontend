8d0c816671b22be1d76869ecaf1eb8c42645bb74
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-06 13:31:22 +0200

    Weltkarte färbt besuchte Länder basierend auf Trip-Daten ein

--- src/app/world-map/world-map.ts
+++ src/app/world-map/world-map.ts
@@ -1,6 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
+import { Trip } from '../trip';
+
+const countryTranslations: any = {
+  'Deutschland': 'Germany',
+  'Italien': 'Italy',
+  'Frankreich': 'France',
+  'Spanien': 'Spain',
+  'Portugal': 'Portugal',
+  'Griechenland': 'Greece',
+  'Norwegen': 'Norway',
+  'Schweden': 'Sweden',
+  'Dänemark': 'Denmark',
+  'Finnland': 'Finland',
+  'Niederlande': 'Netherlands',
+  'Belgien': 'Belgium',
+  'Österreich': 'Austria',
+  'Schweiz': 'Switzerland',
+  'Polen': 'Poland',
+  'Tschechien': 'Czech Republic',
+  'Kroatien': 'Croatia',
+  'Ungarn': 'Hungary',
+  'Irland': 'Ireland',
+  'Vereinigtes Königreich': 'United Kingdom',
+  'Island': 'Iceland',
+  'Türkei': 'Turkey',
+  'USA': 'United States of America',
+  'Thailand': 'Thailand',
+  'Japan': 'Japan',
+};
 
 @Component({
   selector: 'app-world-map',
@@ -9,7 +38,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './world-map.css',
 })
 export class WorldMap implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private tripService: Trip) {}
 
   ngOnInit() {
     const map = L.map('map').setView([51.1657, 10.4515], 4);
@@ -17,9 +46,23 @@ export class WorldMap implements OnInit {
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
       attribution: '© OpenStreetMap contributors'
     }).addTo(map);
+
+    this.tripService.getTrips().subscribe((trips: any) => {
+      const visitedCountries = trips.map((trip: any) => countryTranslations[trip.country]);
     
-    this.http.get('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').subscribe((geoData: any) => { 
-      L.geoJSON(geoData).addTo(map);
-    });
+      this.http.get('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').subscribe((geoData: any) => { 
+        L.geoJSON(geoData, {
+          style: (feature: any) => {
+            const isVisited = visitedCountries.includes(feature.properties.name);
+            return {
+              fillColor: isVisited ? '#f8bbd0' : '#cccccc',
+              fillOpacity: isVisited ? 0.6 : 0.1,
+              color: '#666',
+              weight: 1
+            };
+          }
+        }).addTo(map);
+      });
+    });  
   }
 }
