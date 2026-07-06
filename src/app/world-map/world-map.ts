d82ba12b69dd656ea6d48a87156898e173abd1c9
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-06 14:06:40 +0200

    Stadt-Pins mit Geocoding und Popup-Info zur Weltkarte hinzugefügt

--- src/app/world-map/world-map.ts
+++ src/app/world-map/world-map.ts
@@ -31,6 +31,13 @@ const countryTranslations: any = {
   'Japan': 'Japan',
 };
 
+const defaultIcon = L.icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41]
+});
+
 @Component({
   selector: 'app-world-map',
   imports: [],
@@ -49,6 +56,15 @@ export class WorldMap implements OnInit {
 
     this.tripService.getTrips().subscribe((trips: any) => {
       const visitedCountries = trips.map((trip: any) => countryTranslations[trip.country]);
+      trips.forEach((trip: any) => {
+        this.http.get(`https://nominatim.openstreetmap.org/search?q=${trip.city}&format=json`).subscribe((results:any) => {
+          if (results.length > 0) {
+            const lat = results[0].lat;
+            const lon = results[0].lon;
+            L.marker([lat, lon], { icon: defaultIcon }).addTo(map).bindPopup(trip.title);
+          }
+        });
+      });
     
       this.http.get('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').subscribe((geoData: any) => { 
         L.geoJSON(geoData, {
@@ -66,3 +82,5 @@ export class WorldMap implements OnInit {
     });  
   }
 }
+
+
