ba045677d37f71a9ce11b367da3a16cc2ea41a23
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-16 17:36:02 +0200

    Mehrere Stationen pro Reise implementiert

--- src/app/trip-form/trip-form.ts
+++ src/app/trip-form/trip-form.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit} from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { Trip } from '../trip';
-import { transformation } from 'leaflet';
 
 @Component({
   selector: 'app-trip-form',
@@ -18,8 +17,9 @@ export class TripForm implements OnInit{
     this.tripForm = this.fb.group({
       title: ['', Validators.required],
       status: ['geplant', Validators.required],
-      country: ['', Validators.required],
-      city: ['', Validators.required],
+      stops: this.fb.array([
+        this.createStop()
+      ]),
       startDate: [''],
       endDate: [''],
       description: [''],
@@ -29,51 +29,87 @@ export class TripForm implements OnInit{
     })
   }
 
+  createStop(): FormGroup {
+    return this.fb.group({
+      country: ['', Validators.required],
+      city: ['', Validators.required]
+    });
+  }
+
+  get stops(): FormArray {
+    return this.tripForm.get('stops') as FormArray;
+  }
+
+  addStop() {
+    this.stops.push(this.createStop());
+  }
+
+  removeStop(index: number) {
+    if (this.stops.length > 1) {
+      this.stops.removeAt(index);
+    }
+  }
+
   ngOnInit(): void {
     this.tripService.tripToEdit.subscribe((trip: any) => {
       this.editingTrip = trip;
+
       this.tripForm.patchValue({
-        ...trip,
-        country: trip.stops?.[0]?.country || '',
-        city: trip.stops?.[0]?.city || ''
+       title: trip.title,
+       status: trip.status,
+       startDate: trip.startDate,
+       endDate: trip.endDate,
+       description: trip.description,
+       travelWith: trip.travelWith,
+       transportMode: trip.transportMode,
+       rating: trip.rating
       });
+
+      this.stops.clear();
+
+      trip.stops.forEach((stop: any) => {
+        this.stops.push(
+          this.fb.group({
+            country: [stop.country, Validators.required],
+            city: [stop.city, Validators.required]
+          })
+        )
+      })
     });
   }
 
   onSubmit() {
-    const formValue = this.tripForm.value;
-
-    const tripData = {
-      title: formValue.title,
-      status: formValue.status,
-      startDate: formValue.startDate,
-      endDate: formValue.endDate,
-      description: formValue.description,
-      travelWith: formValue.travelWith,
-      transportMode: formValue.transportMode,
-      rating: formValue.rating,
-      stops: [
-        {
-          country: formValue.country,
-          city: formValue.city
-        }
-      ]
+    if (this.tripForm.invalid) {
+      return;
     }
-    
+
     if (this.editingTrip) {
-      this.tripService.updateTrip(this.editingTrip._id, this.tripForm.value).subscribe(() => {
-        console.log('trip aktualisiert!');
+      this.tripService
+      .updateTrip(this.editingTrip._id, this.tripForm.value)
+      .subscribe(() => {
+        console.log('Trip aktualisiert!');
         this.tripService.tripCreated.next();
         this.editingTrip = null;
         this.tripForm.reset();
       });
     } else {
-      this.tripService.createTrip(tripData).subscribe(() => {
+      this.tripService
+      .createTrip(this.tripForm.value)
+      .subscribe(() => {
         console.log('Trip erstellt!');
         this.tripService.tripCreated.next();
         this.tripForm.reset();
       }); 
     }
+  }
+
+  resetForm() {
+    this.tripForm.reset({
+      status: 'geplant'
+    });
+
+    this.stops.clear();
+    this.stops.push(this.createStop());
   }
   
 }
