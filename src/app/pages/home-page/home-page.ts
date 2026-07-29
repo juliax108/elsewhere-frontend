0e9c510432e19b94e418f13f5254deb6b0f8c6ee
Julia König <Julia.Koenig@Student.HTW-Berlin.de>
2026-07-29 19:26:56 +0200

    Startseite mit Weltkarte und Statistik aufgebaut

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Statistics } from '../../statistics/statistics';
+import { WorldMap } from '../../world-map/world-map';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Statistics],
+  imports: [Statistics, WorldMap],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
